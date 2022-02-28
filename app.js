b9c976c7c8827860c6f73d7d9159767579a23d8e
Fahad-Faisaal <fahads1ab@gmail.com>
2022-02-28 19:16:40 +0600

    initial loading with data added

---
+++ app.js
@@ -0,0 +1,33 @@
+const toggleSpinner = displayClass => document.getElementById('spinner').classList.add(displayClass);
+
+// toggleSpinner('d-none');
+
+const loadInitialDrinks = async function(){
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink');
+
+  const data = await response.json();
+
+  displayInitialDrinks(data.drinks)
+}
+
+loadInitialDrinks();
+
+const displayInitialDrinks = function(drink){
+  const drinkContainer = document.getElementById('drink-container');
+  drink.forEach(drink => {
+    console.log(drink);
+    const card = document.createElement('div');
+    card.classList.add('col');
+
+    card.innerHTML = `
+    <div class="card bg-dark text-black h-100 position-relative">
+      <img src=${drink.strDrinkThumb} alt="drink-image">
+      <div class="card-img-overlay">
+        <h5 class="card-title fw-bolder position-absolute bottom-0 end-0 mb-3 me-2 h4">${drink.strDrink}</h5>
+      </div>
+     </div>
+    `;
+
+    drinkContainer.appendChild(card);
+  })
+}
